202b2a3688a5349de6b203a2be37755e8f3d3461
Youssef Emad <youssefweb120@gmail.com>
2023-07-30 02:45:12 +0300

    edit some files

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 async function getData(id) {
   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
@@ -15,46 +17,61 @@ async function getData(id) {
   return res.json();
 }
 
-export async function generateMetadata({ params }) {
-  const post = await getData(params.id);
-  return {
-    title: post.title,
-    description: post.desc,
-  };
-}
+// export async function generateMetadata({ params }) {
+//   const post = await getData(params.id);
+//   return {
+//     title: post.title,
+//     description: post.desc,
+//   };
+// }
 
-const BlogPost = async ({ params }) => {
-  const data = await getData(params.id);
+const BlogPost =  ({ params }) => {
+  const { data: session } = useSession();
+  const [myPosts, setMyPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/posts/${params.id}`, {
+        cache: "no-store",
+      });
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user) fetchPosts();
+  }, [session]);
+  // const data = await getData();
+  console.log(myPosts);
+  // const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>{data.desc}</p>
+          <h1 className={styles.title}>{myPosts.title}</h1>
+          <p className={styles.desc}>{myPosts.desc}</p>
           <div className={styles.author}>
             <Image
-              src={data.img}
+              src={myPosts.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>{data.username}</span>
+            <span className={styles.username}>{myPosts.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={data.img} alt="" fill={true} className={styles.image} />
+          <Image src={myPosts.img} alt="" fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>{data.content}</p>
+        <p className={styles.text}>{myPosts.content}</p>
       </div>
     </div>
   );
 };
 
 export default BlogPost;
-
 
 // import React from 'react'
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
+// async function getData() {
+//   const res = await fetch(`/app/api/posts`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
-const Blog = async () => {
-  const data = await getData();
+const Blog = () => {
+  const { data: session } = useSession();
+  const [myPosts, setMyPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/posts`, {
+        cache: "no-store",
+      });
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user) fetchPosts();
+  }, [session]);
+  // const data = await getData();
+  console.log(myPosts);
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
+      {myPosts.map((item) => (
         <Link
           href={`/blog/${item._id}`}
           className={styles.container}
-          key={item.id}
+          key={item._id}
         >
           <div className={styles.imageContainer}>
             <Image
@@ -45,7 +62,6 @@ const Blog = async () => {
 };
 
 export default Blog;
-
 
 // import React from 'react'
 
